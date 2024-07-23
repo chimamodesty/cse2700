e1cff3cd0b6a1759777310336fadf92e18622baa
chimamodesty <chimavic2000@gmail.com>
2024-07-24 00:59:59 +0100

    another commit

--- tests/test_testtwo.py
+++ tests/test_testtwo.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 class TestTesttwo():
   options = Options()
