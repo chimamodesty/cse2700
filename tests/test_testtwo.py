e75672662d3ecae409d9d718a7ce527cf2dbb283
chimamodesty <chimavic2000@gmail.com>
2024-07-24 00:58:04 +0100

    second commit

--- tests/test_testtwo.py
+++ tests/test_testtwo.py
@@ -11,9 +11,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestTesttwo():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
+  options = Options()
+  options.add_argument("--headless=new")
+  self.driver = webdriver.Chrome(options=options)
+  self.vars = {}
+
   
   def teardown_method(self, method):
     self.driver.quit()
